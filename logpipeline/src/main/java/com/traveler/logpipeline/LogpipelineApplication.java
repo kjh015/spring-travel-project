bc36b104a8c0194b5c5b73c276c5f1845d6673ad
kjh015 <khj843@naver.com>
2025-05-04 17:51:39 +0900

    .

---
+++ logpipeline/src/main/java/com/traveler/logpipeline/LogpipelineApplication.java
@@ -0,0 +1,13 @@
+package com.traveler.logpipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogpipelineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogpipelineApplication.class, args);
+	}
+
+}
