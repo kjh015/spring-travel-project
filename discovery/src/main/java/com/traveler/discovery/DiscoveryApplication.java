984d92d040951734b94af9e4aea89f15e8570a7f
kjh015 <khj843@naver.com>
2025-06-27 17:46:36 +0900

    06/27 6

--- discovery/src/main/java/com/traveler/discovery/DiscoveryApplication.java
+++ discovery/src/main/java/com/traveler/discovery/DiscoveryApplication.java
@@ -4,11 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 @EnableEurekaServer
 public class DiscoveryApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
 		SpringApplication.run(DiscoveryApplication.class, args);
 	}
 
