3084d3049c7ec6b4d331c22a3fa1bfd7186cb763
kjh015 <khj843@naver.com>
2025-06-27 17:31:54 +0900

    06/27 5

--- sign/src/main/java/com/traveler/sign/SignApplication.java
+++ sign/src/main/java/com/traveler/sign/SignApplication.java
@@ -4,11 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 @EnableDiscoveryClient
 public class SignApplication {
 
+
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
 		SpringApplication.run(SignApplication.class, args);
 	}
 
