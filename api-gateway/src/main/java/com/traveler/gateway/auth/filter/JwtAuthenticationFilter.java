be2aa5b5a657702a0c6ac7915ff95945106265f3
kjh015 <khj843@naver.com>
2026-05-10 13:36:20 +0900

    ♻️ Refactor: JwtFilter에 excludePaths 삭제

--- api-gateway/src/main/java/com/traveler/gateway/auth/filter/JwtAuthenticationFilter.java
+++ api-gateway/src/main/java/com/traveler/gateway/auth/filter/JwtAuthenticationFilter.java
@@ -7,18 +7,13 @@ import com.traveler.gateway.auth.support.JwtTokenProvider;
 import com.traveler.gateway.auth.support.TokenBlacklistManager;
 import com.traveler.gateway.exception.ApiGatewayErrorCode;
 import com.traveler.gateway.exception.ApiGatewayNoStackException;
-import java.util.Collections;
-import java.util.List;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
-import org.springframework.http.server.PathContainer;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
-import org.springframework.web.util.pattern.PathPattern;
-import org.springframework.web.util.pattern.PathPatternParser;
 import reactor.core.publisher.Mono;
 
 @Slf4j
@@ -40,34 +35,20 @@ public class JwtAuthenticationFilter extends AbstractGatewayFilterFactory<JwtAut
     }
 
     @Data
-    public static class Config {
-        private List<String> excludePaths;
-    }
+    public static class Config {}
 
     @Override
     public GatewayFilter apply(Config config) {
-        List<PathPattern> patterns = parsePatterns(config.getExcludePaths());
-
-        return (exchange, chain) -> {
-            var path = exchange.getRequest().getPath().pathWithinApplication();
-
-            // 제외 경로 확인
-            if (isExcluded(path, patterns)) {
-                return chain.filter(authContextManager.prepareAuthorizedExchange(exchange, null));
-            }
-
-            return Mono.justOrEmpty(resolveToken(exchange.getRequest()))
-                    .switchIfEmpty(Mono.error(new ApiGatewayNoStackException(ApiGatewayErrorCode.INVALID_TOKEN_TYPE)))
-                    .flatMap(
-                            token -> tokenBlacklistManager.checkBlacklist(token).then(Mono.just(token)))
-                    .flatMap(jwtTokenProvider::validateToken)
-                    .map(claims -> UserContext.of(Long.valueOf(claims.getSubject()), jwtTokenProvider.getRoles(claims)))
-                    .flatMap(authUser -> {
-                        // 컨텍스트 저장 및 헤더 주입
-                        authContextManager.storeAuthenticatedUser(exchange, authUser);
-                        return chain.filter(authContextManager.prepareAuthorizedExchange(exchange, authUser));
-                    });
-        };
+        return (exchange, chain) -> Mono.justOrEmpty(resolveToken(exchange.getRequest()))
+                .switchIfEmpty(Mono.error(new ApiGatewayNoStackException(ApiGatewayErrorCode.INVALID_TOKEN_TYPE)))
+                .flatMap(token -> tokenBlacklistManager.checkBlacklist(token).then(Mono.just(token)))
+                .flatMap(jwtTokenProvider::validateToken)
+                .map(claims -> UserContext.of(Long.valueOf(claims.getSubject()), jwtTokenProvider.getRoles(claims)))
+                .flatMap(authUser -> {
+                    // 컨텍스트 저장 및 헤더 주입
+                    authContextManager.storeAuthenticatedUser(exchange, authUser);
+                    return chain.filter(authContextManager.prepareAuthorizedExchange(exchange, authUser));
+                });
     }
 
     private String resolveToken(ServerHttpRequest request) {
@@ -76,15 +57,5 @@ public class JwtAuthenticationFilter extends AbstractGatewayFilterFactory<JwtAut
             return bearerToken.substring(7);
         }
         return null;
-    }
-
-    private List<PathPattern> parsePatterns(List<String> paths) {
-        if (paths == null) return Collections.emptyList();
-        PathPatternParser parser = new PathPatternParser();
-        return paths.stream().map(parser::parse).toList();
-    }
-
-    private boolean isExcluded(PathContainer path, List<PathPattern> patterns) {
-        return patterns.stream().anyMatch(pattern -> pattern.matches(path));
     }
 }
