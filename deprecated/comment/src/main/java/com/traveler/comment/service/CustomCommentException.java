2e3a48a4bf48a8a37ae142dd0f2a4fb6064a3555
kjh015 <khj843@naver.com>
2026-05-11 17:47:05 +0900

    📝 Chore: .gitignore에 deprecated 폴더 추가

--- deprecated/comment/src/main/java/com/traveler/comment/service/CustomCommentException.java
+++
@@ -1,7 +0,0 @@
-package com.traveler.comment.service;
-
-public class CustomCommentException extends RuntimeException {
-    public CustomCommentException(String message) {
-        super(message);
-    }
-}
--- deprecated/logpipeline/src/main/java/com/traveler/logpipeline/repository/ProcessRepository.java
+++
@@ -1,6 +0,0 @@
-package com.traveler.logpipeline.repository;
-
-import com.traveler.logpipeline.entity.Process;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProcessRepository extends JpaRepository<Process, Long> {}
